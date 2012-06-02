fe4cbbd76190fb957e9d0c95d2d1593ff2018781
attdevsupport <developer.support@att.com>
2012-06-02 15:34:43 -0700

    Incremental changes to payment pages

--- Payment/main.js
+++ Payment/main.js
@@ -1,47 +1,31 @@
 // I used YUI 3, so you can google their documentation to find other capabilites, otherwise I'll just do it afterwards.
 YUI().use("io-base", "cache-offline", "node", "transition", "node-load", "get", "gallery-dispatcher", function(Y) {
     
+    Y.all('#menu-bar').hide();
     function onLoad(id, o) {
         var data = o.responseText; // Response data.
-        Y.one('body').append('<div style="visibility:hidden;position:absolute;top:120px;left:0px;'
+        Y.one('body').append('<div style="visibility:hidden;position:absolute;top:110px;left:0px;'
                                 +'width:100%;height:80%;" id="' + uri + '">' + data);
         Y.one('body').append('</div>');
     };
     
     Y.on('io:complete', onLoad, Y);
     
-    var uri = "specials.html";
+    var uri = "thanks-for-playing.html";
     var request = Y.io(uri, {sync:true});
-    var uri = "desserts.html";
-    var request = Y.io(uri, {sync:true});
-    var uri = "drinks.html";
-    var request = Y.io(uri, {sync:true});
-    var uri = "food.html";
+    var uri = "thank-you.html";
     var request = Y.io(uri, {sync:true});
     var uri = "not-supported.html";
     var request = Y.io(uri, {sync:true});
     var uri = "onourway.html";
     var request = Y.io(uri, {sync:true});
-    var uri = "other.html";
-    var request = Y.io(uri, {sync:true});
     var uri = "please-swipe.html";
-    var request = Y.io(uri, {sync:true});
-    var uri = "popular.html";
-    var request = Y.io(uri, {sync:true});
-    var uri = "popup.html";
-    var request = Y.io(uri, {sync:true});
-    var uri = "remembered.html";
-    var request = Y.io(uri, {sync:true});
-    var uri = "thank-you.html";
-    var request = Y.io(uri, {sync:true});
-    var uri = "thanks-for-playing.html";
     var request = Y.io(uri, {sync:true});
     var uri = "tips.html";
     var request = Y.io(uri, {sync:true});
-
-    var uri = "specials-sundae.html";
+    var uri = "popup.html";
     var request = Y.io(uri, {sync:true});
-    var uri = "specials-turkey.html";
+    var uri = "bill.html";
     var request = Y.io(uri, {sync:true});
         
     // Set up cache for saving variables
@@ -50,6 +34,9 @@ YUI().use("io-base", "cache-offline", "node", "transition", "node-load", "get", 
         uniqueKeys: true,
         expires:21600000 // Expire data after 6 hours
     });
+    
+    // Clear cache during development only
+    cache.flush();
     
     // Check if Remembered button should be lit up, by checking for key in cache
     if(cache.retrieve("RememberedButtonLitUp")) {
@@ -72,16 +59,22 @@ YUI().use("io-base", "cache-offline", "node", "transition", "node-load", "get", 
     // Check what page is loaded in main content div, by checking for key in cache, and then load that page
     if(cache.retrieve("CurrentlyLoadedPage")) {
         document.getElementById(cache.retrieve("CurrentlyLoadedPage").response).style.visibility = 'visible';
+        if(cache.retrieve("CurrentlyLoadedPage").response.split("details").length==1) {
+            Y.all('#back').hide();
+        } else {
+            Y.all('#menu-bar').hide();
+        }
     } else {
         // If it has not been set, then set to default
-        cache.add("CurrentlyLoadedPage", "specials-royale.html");
+        cache.add("CurrentlyLoadedPage", "bill.html");
         //Y.one('#content').load(cache.retrieve("CurrentlyLoadedPage").response);
         document.getElementById(cache.retrieve("CurrentlyLoadedPage").response).style.visibility = 'visible';
+        Y.all('#back').hide();
     }
     
     // Set default LastLoadedPage to default specials page
     if(!cache.retrieve("LastLoadedPage")) {
-        cache.add("LastLoadedPage", "specials-royale.html");
+        cache.add("LastLoadedPage", "bill.html");
     }
 
     // This is how you add entries to the Cache
@@ -162,6 +155,72 @@ YUI().use("io-base", "cache-offline", "node", "transition", "node-load", "get", 
         //Y.one('#content').load(node.get("name"), function() {});
         document.getElementById(cache.retrieve("LastLoadedPage").response).style.visibility = 'hidden';
         document.getElementById(node.get("name")).style.visibility = 'visible';
+        
+        // Manipulate menu and back button
+//        if(cache.retrieve("CurrentlyLoadedPage").response.split("details").length>1) {
+//            Y.all('#menu-bar').hide();
+//            Y.all('#back').show();
+//        }
+//        if(cache.retrieve("CurrentlyLoadedPage").response.split("details").length==1) {
+//            Y.all('#back').hide();
+//            Y.all('#menu-bar').show();
+//        }
+        
+        if(node.getAttribute("class")=="menu-item") {
+            Y.all('#load-new-page').each(function(node) {
+                if(node.get('parentNode').get('id')=='menu-bar') {
+                    node.removeClass("lit-up");
+                }
+            });
+            node.setAttribute("class", "menu-item lit-up");
+        }
+        
+        if(node.get("name")=="tips.html") {
+            Y.all('#back').show();
+        }
+    });
+    
+    // Load new special
+    Y.all('#load-new-special').on('click', function (e) {
+        var node = e.currentTarget;
+        // Set cache key LastLoadedPage to last page
+        cache.add("LastLoadedPage", cache.retrieve("CurrentlyLoadedPage").response);
+        // Set cache key CurrentlyLoadedPage to new page
+        cache.add("CurrentlyLoadedPage", node.get("name"));
+        // Load page
+        //Y.one('#content').load(node.get("name"), function() {});
+        document.getElementById(cache.retrieve("LastLoadedPage").response).style.visibility = 'hidden';
+        document.getElementById(node.get("name")).style.visibility = 'visible';
+        
+        //Change Specials button to come back to this specific special
+        Y.all('#load-new-page').each(function(node) {
+                if(node.get('parentNode').get('id')=='menu-bar') {
+                    if(node.get("name").split("specials").length==1) {
+                        node.removeClass("lit-up");
+                    }
+                    if(node.get("name").split("specials").length>1) {
+                        node.setAttribute("name", e.currentTarget.get("name"));
+                    }
+                }
+            });
+        
+        // Manipulate menu and back button
+        if(cache.retrieve("CurrentlyLoadedPage").response.split("details").length>1) {
+            Y.all('#menu-bar').hide();
+            Y.all('#back').show();
+        }
+        if(cache.retrieve("CurrentlyLoadedPage").response.split("details").length==1) {
+            Y.all('#back').hide();
+            Y.all('#menu-bar').show();
+        }
+        
+        // Light up clicked menu button, take dim the others
+        if(node.getAttribute("class")=="menu-item") {
+            Y.all('#load-new-page').each(function(node) {
+                node.removeClass("lit-up");
+            });
+            node.setAttribute("class", "menu-item lit-up");
+        }
     });
     
     // Load last page (Back)
@@ -171,6 +230,26 @@ YUI().use("io-base", "cache-offline", "node", "transition", "node-load", "get", 
         document.getElementById(cache.retrieve("LastLoadedPage").response).style.visibility = 'visible';
         cache.add("CurrentlyLoadedPage", cache.retrieve("LastLoadedPage").response);
         cache.add("LastLoadedPage", "specials-royale.html");
+        
+        Y.all('#back').hide();
+        
+        // Manipulate menu and back button
+//        if(cache.retrieve("CurrentlyLoadedPage").response.split("details").length>1) {
+//            Y.all('#menu-bar').hide();
+//            Y.all('#back').show();
+//        }
+//        if(cache.retrieve("CurrentlyLoadedPage").response.split("details").length==1) {
+//            Y.all('#back').hide();
+//            Y.all('#menu-bar').show();
+//        }
+    });
+    
+    // Split bill
+    Y.all('#split').on('click', function (e) {
+        Y.all('#checkbox').show();
+        Y.all('#split').setAttribute("name", "tips.html");
+        Y.all('#split').innerHTML = 'test';
+        Y.all('#split').setAttribute("id", "load-new-page");
     });
     
     // Close popup
@@ -189,8 +268,9 @@ YUI().use("io-base", "cache-offline", "node", "transition", "node-load", "get", 
             document.getElementById('close-popup').onclick = closePopup;
             // If loading the Call Waiter popup, then light up the Call button and 
             // save that to the cache, so it stays lit.
-            if(e.currentTarget.get("name")=="onourway.html") {
-                e.currentTarget.setAttribute("class", "button lit-up");
+            if((e.currentTarget.get("name")=="onourway.html") || (e.currentTarget.get("name")=="onourway2.html")) {
+                var oow = document.getElementsByName('onourway.html')[0];
+                oow.setAttribute("class", "button lit-up");
                 cache.add("CallButtonLitUp", true);
             }
         });
@@ -199,10 +279,29 @@ YUI().use("io-base", "cache-offline", "node", "transition", "node-load", "get", 
     // Load Remembered list
     Y.all('#remembered').on('click', function (e) {
         //Y.one('#content').load('remembered.html');
+        Y.all('#menu-bar').show();
+        Y.all('#back').hide();
         document.getElementById(cache.retrieve("CurrentlyLoadedPage").response).style.visibility = 'hidden';
         document.getElementById(e.currentTarget.get("name")).style.visibility = 'visible';
         cache.add("LastLoadedPage", cache.retrieve("CurrentlyLoadedPage").response);
         cache.add("CurrentlyLoadedPage", "remembered.html");
+        var ri = cache.retrieve("RememberedItems");
+        if(ri==null)
+            ri = '';
+        else
+            ri = cache.retrieve("RememberedItems").response;
+        if(ri.split('bigmac').length==1)
+            Y.all('#bigmac-remembered').hide();
+        else
+            Y.all('#bigmac-remembered').show();
+        if(ri.split('royale').length==1)
+            Y.all('#royale-remembered').hide();
+        else
+            Y.all('#royale-remembered').show();
+        if(ri.split('fries').length==1)
+            Y.all('#fries-remembered').hide();
+        else
+            Y.all('#fries-remembered').show();
     });
     
     // Example of clicking a Remember button, which lights up the 
@@ -211,6 +310,7 @@ YUI().use("io-base", "cache-offline", "node", "transition", "node-load", "get", 
         // Light up remembered button
         var remembered = Y.one('#remembered');
         remembered.setAttribute("class", "button lit-up");
+        e.currentTarget.setAttribute("class", "button lit-up");
         
         // Add key in cache that Remembered button is now lit up
         cache.add("RememberedButtonLitUp", true);
@@ -227,5 +327,41 @@ YUI().use("io-base", "cache-offline", "node", "transition", "node-load", "get", 
             cache.add("RememberedItems", RememberedItems);
         }
         
+    });
+    // Forget item
+    Y.all('#forget').on('click', function(e) {
+        var node = e.currentTarget;
+        var RememberedItems = cache.retrieve("RememberedItems");
+        if(RememberedItems!=null) {
+            RememberedItems = RememberedItems.response;
+            if(RememberedItems.split(node.get("name")).length>1) {
+                RememberedItems = RememberedItems.replace(node.get("name"), '').replace(',,', ',');
+                cache.add("RememberedItems", RememberedItems);
+            }
+        }
+        Y.all('#remember').each(function(n) {
+            if(n.get("name")==node.get("name")) {
+                n.setAttribute("class", "button");
+            }
+        });
+        if(RememberedItems.length<4)
+            Y.all('#remembered').setAttribute("class", "button");
+        var ri = cache.retrieve("RememberedItems");
+        if(ri==null)
+            ri = '';
+        else
+            ri = cache.retrieve("RememberedItems").response;
+        if(ri.split('bigmac').length==1)
+            Y.all('#bigmac-remembered').hide();
+        else
+            Y.all('#bigmac-remembered').show();
+        if(ri.split('royale').length==1)
+            Y.all('#royale-remembered').hide();
+        else
+            Y.all('#royale-remembered').show();
+        if(ri.split('fries').length==1)
+            Y.all('#fries-remembered').hide();
+        else
+            Y.all('#fries-remembered').show();
     });
 });
